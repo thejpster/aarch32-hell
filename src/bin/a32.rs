e499fd51269985da475944a6c0469eef95070a73
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2026-02-01 10:09:50 +0000

    Add actual global_asm example.

--- src/bin/a32.rs
+++ src/bin/a32.rs
@@ -1,25 +1,44 @@
 #![no_std]
 #![no_main]
 
+use core::arch::global_asm;
+
 #[unsafe(no_mangle)]
 pub fn entry() {
-    naked();
-    globalasm();
+    nakedfn();
+    normalfn();
+    globalfn();
 }
 
 #[unsafe(naked)]
 #[instruction_set(arm::a32)]
-extern "C" fn naked() {
-    core::arch::naked_asm!(
-        "nop",
-        "bx lr",
-    );
+extern "C" fn nakedfn() {
+    core::arch::naked_asm!("nop", "bx lr",);
 }
 
 #[instruction_set(arm::a32)]
-extern "C" fn globalasm() {
-    unsafe { core::arch::asm!("nop"); }
+extern "C" fn normalfn() {
+    unsafe {
+        core::arch::asm!("nop");
+    }
 }
+
+unsafe extern "C" {
+    safe fn globalfn();
+}
+
+global_asm!(
+    r#"
+    .arm
+    .global globalfn
+    .type globalfn, %function
+    globalfn:
+        push    {{ r7, lr }}
+        nop
+        pop     {{ r7, pc }}
+    .size globalfn, . - globalfn
+"#
+);
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
